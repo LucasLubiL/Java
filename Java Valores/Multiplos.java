18b63308a84624829ba929bbd45328fb5c0ab2a0
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-04-21 18:29:13 -0300

    Add files via upload

---
+++ Java Valores/Multiplos.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class multiplos {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        int x,y;
+        double soma;
+
+        x=sc.nextInt();
+        y=sc.nextInt();
+
+        if(x>y){
+           soma=x%y;
+           if(soma==0){
+              System.out.println("Sao Multiplos");
+           }else{
+              System.out.println("Nao sao Multiplos");
+           }
+        }else{
+            soma=y%x;
+           if(soma==0){
+              System.out.println("Sao Multiplos");
+           }else{
+              System.out.println("Nao sao Multiplos");
+           }
+        }
+        
+        sc.close();
+
+    }
+    
+}
