05882d37849e7e8aada8b5c65efa6365d61e602b
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-04-21 19:16:24 -0300

    Add files via upload

---
+++ Java Valores/dddBEECROWD1050.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class dddBEECROWD1050 {
+    public static void main(String[] args) {
+        
+    Locale.setDefault(Locale.US);
+
+    Scanner sc =new Scanner(System.in);
+
+    int a;
+
+    a=sc.nextInt();
+
+    if(a==61){
+        System.out.println("Brasilia");
+    }else if(a==71){
+        System.out.println("Salvador");
+    }else if(a==11){
+        System.out.println("Sao Paulo");
+    }else if(a==21){
+        System.out.println("Rio de Janeiro");
+    }else if(a==32){
+        System.out.println("Juiz de Fora");
+    }else if(a==19){
+        System.out.println("Campinas");
+    }else if(a==27){
+        System.out.println("Vitoria");
+    }else if(a==31){
+        System.out.println("Belo Horizonte");
+    }else{
+        System.out.println("DDD nao cadastrado");
+    }
+    
+    sc.close();
+
+    }
+}
