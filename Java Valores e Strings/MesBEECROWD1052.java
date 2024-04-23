a9a4fe8fda491e0d4a8c55f1fa7c09635cb51a84
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-04-22 22:34:56 -0300

    Add files via upload

---
+++ Java Valores e Strings/MesBEECROWD1052.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MesBEECROWD1052 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc =new Scanner(System.in);
+
+        int a;
+
+        a=sc.nextInt();
+
+        if(a==1){
+            System.out.println("January");
+        }else if(a==2){
+            System.out.println("February");
+        }else if(a==3){
+            System.out.println("March");
+        }else if(a==4){
+            System.out.println("April");
+        }else if(a==5){
+            System.out.println("May");
+        }else if(a==6){
+            System.out.println("June");
+        }else if(a==7){
+            System.out.println("July");
+        }else if(a==8){
+            System.out.println("August");
+        }else if(a==9){
+            System.out.println("September");
+        }else if(a==10){
+            System.out.println("October");
+        }else if(a==11){
+            System.out.println("November");
+        }else if(a==12){
+            System.out.println("December");
+        }
+       
+
+        sc.close();
+
+    }
+    
+}
