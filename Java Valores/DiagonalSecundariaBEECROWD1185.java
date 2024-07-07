1aad8b8216a3806c30a5190f0c293c098a03b316
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-07-07 00:04:28 -0300

    Add files via upload

---
+++ Java Valores/DiagonalSecundariaBEECROWD1185.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DiagonalSecundariaBEECROWD1185
+{
+	public static void main(String[] args) {
+	    
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		double[][] ma= new double[12][12];
+		double soma=0;
+		double media=0;
+        int go=11;
+		char b;
+		
+		b=sc.next().charAt(0);
+		
+		for(int l=0; l<12; l++){
+		    for(int c=0; c<12; c++){
+		        ma[l][c]=sc.nextDouble();
+		    }
+		}
+		
+		for(int l=0; l<12; l++){
+		    
+		    for(int c=0; c<12; c++){
+		        if(c<go){
+		           soma=soma+ma[l][c];
+            
+		        }
+		    }
+		    go=go-1;
+		}
+		
+		if(b=='S'){
+		    System.out.println(soma);
+		}else{
+		    media=(double)soma/66;
+		    System.out.println(String.format("%.1f",media));
+		}
+		
+		sc.close();
+		
+	}
+}
