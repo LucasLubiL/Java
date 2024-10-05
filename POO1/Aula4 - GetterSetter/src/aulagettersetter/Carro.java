21f723d2331d65a705ad313c8d81ebe4a07189bf
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-10-04 22:21:08 -0300

    Add files via upload

--- POO1/Aula4 - GetterSetter/src/aulagettersetter/Carro.java
+++ POO1/Aula4 - GetterSetter/src/aulagettersetter/Carro.java
@@ -1,53 +1,51 @@
-package aulagettersetter;
+package gettersetter;
 
 /**
  *
- * @author IFTM
+ * @author User
  */
 public class Carro {
     
     private String marca;
     private String modelo;
     private int ano;
-    
+    private double preco;
     
     //metodos getters - eles recuperam informações
     //metodos setters - adicionam informações
     
     public String getMarca(){
-        
         return this.marca; 
-        
     }
     
-    public String getModelo(){
-        
+    public String getModelo(){ 
         return this.modelo;
-        
     }
     
-    public int getAno(){
-        
+    public int getAno(){ 
         return this.ano;
-        
     }
     
     public void setMarca(String marca){
-        
         this.marca = marca;
-        
     }
     
     public void setModelo(String modelo){
-     
         this.modelo = modelo;
-        
     }  
     
     public void setAno(int ano){
-        
         this.ano = ano;
-        
     }
-   
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco*0.20+preco;
+    }
+    
+    
+    
 }
