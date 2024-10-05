b96e55572c26b6615788b6d8f06ba1b61560f9f1
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-10-04 22:21:32 -0300

    Add files via upload

--- POO1/Aula4 - GetterSetter/src/aulagettersetter/Principal.java
+++ POO1/Aula4 - GetterSetter/src/aulagettersetter/Principal.java
@@ -1,22 +1,22 @@
-package aulagettersetter;
+package gettersetter;
 
 /**
  *
- * @author IFTM
+ * @author User
  */
 public class Principal {
-
-    
     public static void main(String[] args) {
-        Carro fusca = new Carro();
         
-        fusca.setMarca("Volkswagen");
-        fusca.setModelo("Fusca");
-        fusca.setAno(1982);
+       Carro fusca = new Carro();
         
-        System.out.println("Carro: " + fusca.getMarca() + "\n- " + fusca.getModelo());
-        System.out.println("Ano: " + fusca.getAno());
+       fusca.setMarca("Volkswagen");
+       fusca.setModelo("Fusca");
+       fusca.setAno(1982);
+       fusca.setPreco(7000);
         
+       System.out.println("Carro: " + fusca.getMarca() + " - " + fusca.getModelo());
+       System.out.println("Ano: " + fusca.getAno()); 
+       System.out.println("Valor Tabela Fipe: R$" + fusca.getPreco()); 
     }
-   
+    
 }
