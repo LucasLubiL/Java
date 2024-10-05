6dd459b0983afa47af6dc70e6a75e73c9daa1ca3
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-10-04 22:34:28 -0300

    Add files via upload

---
+++ POO1/Getter and Setter/Carro/src/carro/Carro.java
@@ -0,0 +1,49 @@
+package carro;
+
+/**
+ *
+ * @author User
+ */
+public class Carro {
+    
+    private String marca;
+    private String modelo;
+    private int ano;
+    private double preco;
+    
+    //metodos getters - eles recuperam informações
+    //metodos setters - adicionam informações
+    
+    public String getMarca(){
+        return this.marca; 
+    }
+    
+    public String getModelo(){ 
+        return this.modelo;
+    }
+    
+    public int getAno(){ 
+        return this.ano;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }  
+    
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco*0.20+preco;
+    }
+    
+}
---
+++ POO1/Getter and Setter/Carro/src/carro/Principal.java
@@ -0,0 +1,24 @@
+package carro;
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+       Carro fusca = new Carro();
+        
+       fusca.setMarca("Volkswagen");
+       fusca.setModelo("Fusca");
+       fusca.setAno(1982);
+       fusca.setPreco(7000);
+        
+       System.out.println("Carro: " + fusca.getMarca() + " - " + fusca.getModelo());
+       System.out.println("Ano: " + fusca.getAno()); 
+       System.out.println("Valor Tabela Fipe: R$" + fusca.getPreco()); 
+    
+    }
+    
+}
