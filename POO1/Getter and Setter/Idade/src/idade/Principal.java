a97d90502824708a716911979f6bb6c31130792b
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-10-14 15:15:16 -0300

    Update Principal.java

--- POO1/Getter and Setter/Idade/src/idade/Principal.java
+++ POO1/Getter and Setter/Idade/src/idade/Principal.java
@@ -13,12 +13,16 @@ public class Principal {
         pessoa.setNome("Lucas Amaral");
         pessoa.setSexo('M');
         pessoa.setIdade(22);
-        pessoa.verificarIdade(22);
         System.out.println("Nome: " + pessoa.getNome());
         System.out.println("Sexo: " + pessoa.getSexo());
         System.out.println("Idade: " + pessoa.getIdade());
-        System.out.println(pessoa.verificarIdade(pessoa.getNome));
-        
+        System.out.println(pessoa.verificarIdade() + "\n");
+     
+        Idade pessoa2 = new Idade("Paola",17,'F');
+        System.out.println("Nome: " + pessoa2.getNome());
+        System.out.println("Sexo: " + pessoa2.getSexo());
+        System.out.println("Idade: " + pessoa2.getIdade());
+        System.out.println(pessoa2.verificarIdade());
         
     }
     
