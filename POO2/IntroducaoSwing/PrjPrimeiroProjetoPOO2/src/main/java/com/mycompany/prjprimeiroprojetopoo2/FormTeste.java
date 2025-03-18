e6a5bafc839412b677bf39bdef6b5b106396f3bc
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-03-17 22:14:09 -0300

    Add files via upload

--- POO2/IntroducaoSwing/PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/FormTeste.java
+++ POO2/IntroducaoSwing/PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/FormTeste.java
@@ -28,60 +28,50 @@ public class FormTeste extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        numUm = new javax.swing.JTextField();
+        txtValor2 = new javax.swing.JTextField();
         btnEnviar = new javax.swing.JButton();
         lblRetorno = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        numDois = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-
-        jTextField1.setText("jTextField1");
+        jLabel3 = new javax.swing.JLabel();
+        txtValor1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("FORM TESTE");
 
-        jLabel2.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
-        jLabel2.setText("Digite o primeiro valor");
+        jLabel2.setText("Valor 1");
 
-        numUm.setText("0");
-        numUm.addActionListener(new java.awt.event.ActionListener() {
+        txtValor2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                numUmActionPerformed(evt);
+                txtValor2ActionPerformed(evt);
             }
         });
 
-        btnEnviar.setBackground(new java.awt.Color(0, 153, 0));
-        btnEnviar.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnEnviar.setBackground(new java.awt.Color(0, 0, 0));
+        btnEnviar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         btnEnviar.setForeground(new java.awt.Color(255, 255, 255));
-        btnEnviar.setText("SOMAR");
+        btnEnviar.setText("Somar");
+        btnEnviar.setBorder(javax.swing.BorderFactory.createLineBorder(null));
         btnEnviar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEnviarActionPerformed(evt);
             }
         });
 
-        lblRetorno.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
+        lblRetorno.setBackground(new java.awt.Color(255, 255, 255));
+        lblRetorno.setFont(new java.awt.Font("Segoe UI Black", 0, 36)); // NOI18N
+        lblRetorno.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
-        jLabel5.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
-        jLabel5.setText("Digite o segundo valor");
+        jLabel3.setText("Valor 2");
 
-        numDois.setText("0");
-        numDois.addActionListener(new java.awt.event.ActionListener() {
+        txtValor1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                numDoisActionPerformed(evt);
+                txtValor1ActionPerformed(evt);
             }
         });
-
-        jLabel6.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
-        jLabel6.setText("=");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -89,80 +79,68 @@ public class FormTeste extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(numUm, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jLabel5)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(137, 137, 137)
-                            .addComponent(numDois, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(btnEnviar, javax.swing.GroupLayout.Alignment.TRAILING)))
-                .addGap(18, 18, 18)
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
-                .addComponent(lblRetorno, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(130, 130, 130)
-                .addComponent(jLabel3)
-                .addGap(93, 93, 93)
-                .addComponent(jLabel4)
-                .addContainerGap())
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtValor1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtValor2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnEnviar, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(151, Short.MAX_VALUE)
+                .addComponent(lblRetorno, javax.swing.GroupLayout.PREFERRED_SIZE, 287, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(121, 121, 121))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jLabel1)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(27, 27, 27)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(numUm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel4)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jLabel3))
-                                    .addComponent(jLabel5)
-                                    .addComponent(numDois, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(43, 43, 43)
-                                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(24, 24, 24)
-                        .addComponent(btnEnviar))
+                        .addGap(60, 60, 60)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(txtValor1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(txtValor2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(lblRetorno, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(333, Short.MAX_VALUE))
+                        .addGap(59, 59, 59)
+                        .addComponent(btnEnviar, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 102, Short.MAX_VALUE)
+                .addComponent(lblRetorno, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(62, 62, 62))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void numUmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_numUmActionPerformed
+    private void txtValor2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtValor2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_numUmActionPerformed
+    }//GEN-LAST:event_txtValor2ActionPerformed
 
     private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarActionPerformed
-        String um = numUm.getText();
-        String dois = numDois.getText();
+        /*String valor1 = txtValor1.getText();
+        String valor2 = (txtValor2.getText());*/
         
-        double valor1 = Double.parseDouble(numUm.getText());
-        double valor2 = Double.parseDouble(numDois.getText());
+        double  valor1 = Double.parseDouble(txtValor1.getText());
+        double  valor2 = Double.parseDouble(txtValor2.getText());
+        double resultado = valor1 + valor2;
         
-        lblRetorno.setText(String.valueOf(valor1 + valor2));
-        
+        lblRetorno.setText(String.valueOf(resultado));
     }//GEN-LAST:event_btnEnviarActionPerformed
 
-    private void numDoisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_numDoisActionPerformed
+    private void txtValor1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtValor1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_numDoisActionPerformed
+    }//GEN-LAST:event_txtValor1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -204,12 +182,8 @@ public class FormTeste extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel lblRetorno;
-    private javax.swing.JTextField numDois;
-    private javax.swing.JTextField numUm;
+    private javax.swing.JTextField txtValor1;
+    private javax.swing.JTextField txtValor2;
     // End of variables declaration//GEN-END:variables
 }
--- POO2/IntroducaoSwing/PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/PrjPrimeiroProjetoPOO2.java
+++ POO2/IntroducaoSwing/PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/PrjPrimeiroProjetoPOO2.java
@@ -1,14 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 package com.mycompany.prjprimeiroprojetopoo2;
 
 /**
  *
- * @author Lucas
+ * @author IFTM
  */
 public class PrjPrimeiroProjetoPOO2 {
 
     public static void main(String[] args) {
-        
-        
-        
+        System.out.println("Hello World!");
     }
 }
